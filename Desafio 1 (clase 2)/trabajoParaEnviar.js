1def96aa4f5118ff04104f16e8134171a1a39de2
Martina Beauvais <marbeauvais17@gmail.com>
2022-10-25 12:56:06 -0300

    first commit

---
+++ Desafio 1 (clase 2)/trabajoParaEnviar.js
@@ -0,0 +1,9 @@
+class Usuario{
+    constructor(nombre, apellido, libros, mascotas){
+        this.nombre = nombre, 
+        this.apellido = apellido, 
+        this.libros = libros,
+        this.mascotas = mascotas
+    }
+
+}
